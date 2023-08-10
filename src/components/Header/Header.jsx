3aeb7008dad1eab0291fd42b47daf1f63c987173
Nova <nileshtelang07n5@gmail.com>
2023-08-10 11:58:45 +0530

    "Deployed Successfully"

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,7 +14,7 @@ const Header = ({ onLoad, onPlaceChanged }) => {
         <AppBar position='static'>
             <Toolbar className={classes.toolbar}>
                 <Typography variant='h5' className={classes.title}>
-                    TravelFox
+                    Travel-Fox
                 </Typography>
                 <Box display='flex'>
                     <Typography variant='h6' className={classes.title}>
