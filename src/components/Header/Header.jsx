aa10d395d0b64e8762bab6b3c8a09de3ee8ee12c
Nova <nileshtelang07n5@gmail.com>
2023-08-10 11:15:59 +0530

    "Deployed"

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,7 +14,7 @@ const Header = ({ onLoad, onPlaceChanged }) => {
         <AppBar position='static'>
             <Toolbar className={classes.toolbar}>
                 <Typography variant='h5' className={classes.title}>
-                    Travelify
+                    TravelFox
                 </Typography>
                 <Box display='flex'>
                     <Typography variant='h6' className={classes.title}>
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,60 +1,68 @@
 import React, { useState, useEffect, createRef } from 'react';
 import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
-import PlaceDetails from "../PlaceDetails/PlaceDetails";
 
-import useStyle from "./styles"
+import PlaceDetails from '../PlaceDetails/PlaceDetails';
+import useStyles from './styles.js';
 
-const List = ({ places, childClicked, isLoading, type, setType, rating, setRating }) => {
-    const classes = useStyle();
+const List = ({ places, type, setType, rating, setRating, childClicked, isLoading }) => {
+  const [elRefs, setElRefs] = useState([]);
+  const classes = useStyles();
 
-    const [elRefs, setElRefs] = useState([]);
-    useEffect(() => {
-        setElRefs((refs) => Array(places?.length).fill().map((_, i) => refs[i] || createRef()));
-    }, [places]);
-    
-    return (
-        <div className={classes.container}>
-            <Typography variant='h4'>
-                Restaurants, Hotels & Attractions around you
-            </Typography>
-            {isLoading ? (
-                <div className={classes.loading}>
-                    <CircularProgress size='5rem' />
-                </div>
-            ) : (
-                <>
-                    <FormControl className={classes.formControl}>
-                        <InputLabel>Type</InputLabel>
-                        <Select value={type} onChange={(e) => setType(e.target.value)}>
-                            <MenuItem value='restaurants'>Restaurants</MenuItem>
-                            <MenuItem value='hotels'>Hotels</MenuItem>
-                            <MenuItem value='attractions'>Attractions</MenuItem>
-                        </Select>
-                    </FormControl>
-                    <FormControl className={classes.formControl}>
-                        <InputLabel>Rating</InputLabel>
-                        <Select value={rating} onChange={(e) => setRating(e.target.value)}>
-                            <MenuItem value={0}>All</MenuItem>
-                            <MenuItem value={3}>Above 3.0</MenuItem>
-                            <MenuItem value={4}>Above 4.0</MenuItem>
-                            <MenuItem value={4.5}>Above 4.5</MenuItem>
-                        </Select>
-                    </FormControl>
-                    <Grid container spacing={3} className={classes.list}>
-                        {places?.map((place, i) => (
-                            <Grid ref={elRefs[i]} item key={i} xs={12}>
-                                <PlaceDetails
-                                    place={place}
-                                    selected={Number(childClicked) === i}
-                                    refProp={elRefs[i]}
-                                />
-                            </Grid>
-                        ))}
-                    </Grid>
-                </>
-            )}
+  useEffect(() => {
+    setElRefs((refs) => Array(places.length).fill().map((_, i) => refs[i] || createRef()));
+  }, [places]);
+
+  // Define default values for different types
+  const getDefaultLabel = () => {
+    switch (type) {
+      case 'restaurants':
+        return 'Food & Dining around you';
+      case 'hotels':
+        return 'Rent the Places';
+      case 'attractions':
+        return 'Tourist Spots';
+      default:
+        return '';
+    }
+  };
+
+  return (
+    <div className={classes.container}>
+      <Typography variant="h4">{getDefaultLabel()}</Typography>
+      {isLoading ? (
+        <div className={classes.loading}>
+          <CircularProgress size="5rem" />
         </div>
-    );
+      ) : (
+        <>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="type">Type</InputLabel>
+            <Select id="type" value={type} onChange={(e) => setType(e.target.value)}>
+              <MenuItem value="restaurants">Food & Dining around you</MenuItem>
+              <MenuItem value="hotels">Rent the Places</MenuItem>
+              <MenuItem value="attractions">Tourist Spots</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="rating">Rating</InputLabel>
+            <Select id="rating" value={rating} onChange={(e) => setRating(e.target.value)}>
+              <MenuItem value="">All</MenuItem>
+              <MenuItem value="3">Above 3.0</MenuItem>
+              <MenuItem value="4">Above 4.0</MenuItem>
+              <MenuItem value="4.5">Above 4.5</MenuItem>
+            </Select>
+          </FormControl>
+          <Grid container spacing={3} className={classes.list}>
+            {places?.map((place, i) => (
+              <Grid ref={elRefs[i]} key={i} item xs={12}>
+                <PlaceDetails selected={Number(childClicked) === i} refProp={elRefs[i]} place={place} />
+              </Grid>
+            ))}
+          </Grid>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default List;
