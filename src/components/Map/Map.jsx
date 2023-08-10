93c43388d797d49b1ae2d2e4d476b7c798ad6d2c
Nova <nileshtelang07n5@gmail.com>
2023-08-10 11:50:10 +0530

    Final Product

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,30 +2,32 @@ import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-import { Rating } from '@material-ui/lab';
+import Rating from "@material-ui/lab/Rating";
+import { stylesArray } from './mapStyles';
 
 import useStyles from './styles';
-import mapStyles from './mapStyles'
 
-const Map = ({ setCoordinates, setBounds, coordinates, places, setChildClicked, weatherData}) => {
+export const Map = ({ setCoordinates, setBounds, coordinates, places, setChildClicked, weatherData }) => {
     const classes = useStyles();
-    const isDesktop = useMediaQuery('(min-width: 600px)');
+    const isMobile = useMediaQuery('(min-width: 600px)');
+
     return (
         <div className={classes.mapContainer}>
-            <GoogleMapReact
-                bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
+            <GoogleMapReact 
+                bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }} 
                 defaultCenter={coordinates}
                 center={coordinates}
                 defaultZoom={14}
                 margin={[50, 50, 50, 50]}
-                options={{disableDefaultUI : true, zoomControl : true, styles : mapStyles}}
+                options={{ disableDefaultUI: true, zoomControl: true, styles: stylesArray }}
                 onChange={(e) => {
-                    setCoordinates({ lat: e.center.lat, lng: e.center.lng });
-                    setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+                    setCoordinates({ lat: e.center.lat, lng: e.center.lng })
+                    setBounds({
+                        ne: e.marginBounds.ne,
+                        sw: e.marginBounds.sw
+                    })
                 }}
-                onChildClick={(child) => {
-                    setChildClicked(child)
-                }}
+                onChildClick={(child) => setChildClicked(child)}
             >
                 {places?.map((place, i) => (
                     <div
@@ -35,32 +37,31 @@ const Map = ({ setCoordinates, setBounds, coordinates, places, setChildClicked, 
                         key={i}
                     >
                         {
-                            !isDesktop ? (
-                                <LocationOnOutlinedIcon color='primary' fontSize='large' />
+                            !isMobile ? (
+                                <LocationOnOutlinedIcon color="primary" fontSize="large" />
                             ) : (
                                 <Paper elevation={3} className={classes.paper}>
-                                    <Typography className={classes.typography} variant='subtitle2' gutterBottom>
+                                    <Typography className={classes.typography} variant="subtitle2" gutterBottom>
                                         {place.name}
                                     </Typography>
                                     <img
                                         className={classes.pointer}
-                                        src={place?.photo?.images?.large?.url ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                                        src={place.photo ? place.photo.images.large.url : "https://images.unsplash.com/photo-1555396273-367ea4eb4db5?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmVzdGF1cmFudHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80"}
                                         alt={place.name}
                                     />
-                                    <Rating name="read-only" size="small" value={Number(place.rating)} readOnly />
+                                    <Rating size="small" value={Number(place.rating)} readOnly />
                                 </Paper>
                             )
                         }
                     </div>
                 ))}
-                {weatherData?.list?.map((data, i) => (
-                    <div key={i} lat={data.coord.lat} lng={data.coord.lon}>
-                        <img height='100px' src={`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`} alt={data.weather[0].icon} />
+                
+                 {weatherData?.list?.length && weatherData.list.map((data, i) => (
+                    <div key={i} lat={data.coordinates.lat} lng={data.coordinates.lon}>
+                        <img src={`https://openweathermap.org/img/w/${data.weather[0].icon}.png`} height="70px" alt="weather" />
                     </div>
-                ))}
+                ))} 
             </GoogleMapReact>
         </div>
     );
 };
-
-export default Map;
